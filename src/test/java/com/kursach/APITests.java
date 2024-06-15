36bd83cdd310c2db810a1a1565a0206063b532a4
Padybo <aslbek02@list.ru>
2024-06-15 20:42:48 +0300

    version 1.1

--- src/test/java/com/kursach/APITests.java
+++ src/test/java/com/kursach/APITests.java
@@ -1,14 +1,18 @@
 package com.kursach;
 
+import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.core.Is.is;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
@@ -16,26 +20,25 @@ public class APITests {
 
     @Test
     public void task1() {
-        Response response = given()
+        RestAssured.baseURI = "https://reqres.in/api";
+        UsersPage usersPage = given()
                 .when()
-                .get("https://reqres.in/api/users?page=2")
+                .get("/users?page=2")
                 .then()
+                .log().all()
                 .statusCode(200)
-                .extract()
-                .response();
+                .extract().as(UsersPage.class);
 
-        Map<String, Object> responseBody = response.jsonPath().getMap("");
+        assertEquals(2, usersPage.getPage().intValue(), "Expected page to be 2");
+        assertEquals(2, usersPage.getTotal_pages().intValue(), "Expected total_pages to be 2");
+        assertEquals(12, usersPage.getTotal().intValue(), "Expected total to be 12");
 
-        assertEquals(2, responseBody.get("page"), "Expected page to be 2");
-        assertEquals(2, responseBody.get("total_pages"), "Expected total_pages to be 2");
-        assertEquals(12, responseBody.get("total"), "Expected total to be 12");
+        List<UserData> data = usersPage.getData();
+        List<Integer> ids = data.stream().map(UserData::getId).collect(Collectors.toList());
+        assertEquals(ids.size(), ids.stream().distinct().count(), "Expected IDs to be unique");
 
-        List<Map<String, Object>> data = (List<Map<String, Object>>) responseBody.get("data");
-        List<Integer> ids = data.stream().map(d -> (Integer) d.get("id")).collect(Collectors.toList());
-        assertEquals(ids.size(), ids.stream().distinct().count(), "Ожидаемые идентификаторы должны быть уникальными");
-
-        boolean found = data.stream().anyMatch(d -> "Tobias".equals(d.get("first_name")) && "Funke".equals(d.get("last_name")));
-        assertTrue(found, "Ожидается, что вы найдете пользователя с first_name=Tobias и last_name=Funke");
+        boolean found = data.stream().anyMatch(d -> "Tobias".equals(d.getFirst_name()) && "Funke".equals(d.getLast_name()));
+        assertTrue(found, "Expected to find a user with first_name=Tobias and last_name=Funke");
     }
 
     @Test
@@ -45,6 +48,7 @@ public class APITests {
                         .when()
                         .get("https://reqres.in/api/users/22")
                         .then()
+                        .log().all()
                         .statusCode(404)
                         .extract()
                         .asString();
@@ -53,21 +57,23 @@ public class APITests {
     }
 
     @Test
-    public void testUserDetails() {
-        Response response = given()
+    public void testUserFromResponse() {
+
+        UserData userData = given()
                 .when()
                 .get("https://reqres.in/api/users/2")
                 .then()
+                .log().all()
                 .statusCode(200)
-                .extract().response();
+                .extract().body().jsonPath().getObject("data", UserData.class);
 
-        response.then()
-                .body("data.id", equalTo(2))
-                .body("data.email", equalTo("janet.weaver@reqres.in"))
-                .body("data.first_name", equalTo("Janet"))
-                .body("data.last_name", equalTo("Weaver"))
-                .body("data.avatar", equalTo("https://reqres.in/img/faces/2-image.jpg"));
+        assertThat("User id should be 2", userData.getId(), is(2));
+        assertThat("User email should be janet.weaver@reqres.in", userData.getEmail(), is("janet.weaver@reqres.in"));
+        assertThat("User first name should be Janet", userData.getFirst_name(), is("Janet"));
+        assertThat("User last name should be Weaver", userData.getLast_name(), is("Weaver"));
+        assertThat("User avatar should be correct", userData.getAvatar(), is("https://reqres.in/img/faces/2-image.jpg"));
     }
+
 
     @Test
     public void createUser(){
