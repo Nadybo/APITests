092d46882071cbbe43e1a2ca3fb141455027bb5f
Padybo <aslbek02@list.ru>
2024-06-15 20:50:22 +0300

    version 1.3

--- src/test/java/com/kursach/APITests.java
+++ src/test/java/com/kursach/APITests.java
@@ -1,17 +1,12 @@
 package com.kursach;
 
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.core.Is.is;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
@@ -20,10 +15,9 @@ public class APITests {
 
     @Test
     public void task1() {
-        RestAssured.baseURI = "https://reqres.in/api";
         UsersPage usersPage = given()
                 .when()
-                .get("/users?page=2")
+                .get("https://reqres.in/api/users?page=2")
                 .then()
                 .log().all()
                 .statusCode(200)
@@ -42,6 +36,23 @@ public class APITests {
     }
 
     @Test
+    public void testUserFromResponse() {
+        UserData userData = given()
+                .when()
+                .get("https://reqres.in/api/users/2")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract().body().jsonPath().getObject("data", UserData.class);
+
+        assertThat("Идентификатор пользователя должен быть равен 2", userData.getId(), is(2));
+        assertThat("Адрес электронной почты пользователя должен быть janet.weaver@reqres.in", userData.getEmail(), is("janet.weaver@reqres.in"));
+        assertThat("Имя пользователя должно быть Джанет", userData.getFirst_name(), is("Janet"));
+        assertThat("Фамилия пользователя должна быть Weaver", userData.getLast_name(), is("Weaver"));
+        assertThat("Аватар пользователя должен быть корректным", userData.getAvatar(), is("https://reqres.in/img/faces/2-image.jpg"));
+    }
+
+    @Test
     public void userNotFound() {
         String responseBody =
                 given()
@@ -55,25 +66,6 @@ public class APITests {
 
         assertEquals("{}", responseBody, "Expected response body to be empty JSON object {}");
     }
-
-    @Test
-    public void testUserFromResponse() {
-
-        UserData userData = given()
-                .when()
-                .get("https://reqres.in/api/users/2")
-                .then()
-                .log().all()
-                .statusCode(200)
-                .extract().body().jsonPath().getObject("data", UserData.class);
-
-        assertThat("User id should be 2", userData.getId(), is(2));
-        assertThat("User email should be janet.weaver@reqres.in", userData.getEmail(), is("janet.weaver@reqres.in"));
-        assertThat("User first name should be Janet", userData.getFirst_name(), is("Janet"));
-        assertThat("User last name should be Weaver", userData.getLast_name(), is("Weaver"));
-        assertThat("User avatar should be correct", userData.getAvatar(), is("https://reqres.in/img/faces/2-image.jpg"));
-    }
-
 
     @Test
     public void createUser(){
